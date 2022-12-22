a904b66efed5967e40a92d06d8be15608a0f169d
matanarad <mail2matan@gmail.com>
2022-12-22 22:58:07 +0200

    haarcascade_frontalface_default test

---
+++ locan_comper.py
@@ -0,0 +1,13 @@
+import numpy
+
+coefficient_x = 5
+coefficient_y = 5
+default_x = 1
+default_y = 1
+
+center_x = 0
+center_y = 0
+
+if default_x - center_x >= abs(coefficient_x):
+    if default_y - center_y >= abs(coefficient_y):
+        pass
--- poc.py
+++ poc.py
@@ -1,45 +1,31 @@
+import pathlib
 import cv2
 
-# Open the webcam
-cap = cv2.VideoCapture(0)
-frontal_face_cascade = cv2.CascadeClassifier(
-    "haarcascade_frontalface_default.xml")
-profile_face_cascade = cv2.CascadeClassifier("haarcascade_profileface.xml")
+cascade_path = pathlib.Path(cv2.__file__).parent.absolute(
+) / "data/haarcascade_frontalface_default.xml"
+
+clf = cv2.CascadeClassifier(str(cascade_path))
+
+cam = cv2.VideoCapture(0)
 
 while True:
-    # Take a frame from the webcam
-    ret, frame = cap.read()
+    _, frame = cam.read()
+    frame = cv2.flip(frame, 1)
 
-    # Convert the frame to grayscale
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = clf.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(60, 60),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x, y, width, height) in faces:
+        cv2.rectangle(frame, (x, y), (x+width, y+height), (255, 255, 0), 2)
 
-    # Detect faces in the frame
-    frontal_faces = frontal_face_cascade.detectMultiScale(gray_frame, 1.3, 5)
-    profile_faces = profile_face_cascade.detectMultiScale(gray_frame, 1.3, 5)
-
-    # Iterate over the faces and draw a rectangle around them
-    for (x, y, w, h) in frontal_faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        center_x = (x + w)/2
-        center_y = (y + h)/2
-        if cv2.waitKey(1) & 0xFF == ord("r"):
-            center_x == default_x, default_x
-    for (x, y, w, h) in profile_faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        if cv2.waitKey(1) & 0xFF == ord("5"):
-            print(center_x, center_y)
-
-        # cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
-
-    # Display the frame on the screen
-    cv2.imshow("Live Video with Face Rectangle", frame)
-
-    # Check if the user pressed the 'q' key to quit
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    cv2.imshow("Face", frame)
+    if cv2.waitKey(1) == ord("q"):
         break
 
-# Release the webcam
-cap.release()
-
-# Close all windows
+cam.release()
 cv2.destroyAllWindows()

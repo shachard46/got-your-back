5ffe63d641ff133ffaafa31a873e4238896c7392
shachardavid <shachadavid2013@gmail.com>
2022-12-22 23:50:12 +0200

    face-recognition recognizes faces

--- poc.py
+++ poc.py
@@ -2,6 +2,16 @@ import face_recognition
 import cv2
 import numpy as np
 
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
 
@@ -12,15 +22,30 @@ face_names = []
 process_this_frame = True
 
 while True:
+    # Grab a single frame of video
     ret, frame = video_capture.read()
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, None, fx=0.33, fy=0.33)
 
-    rgb_small_frame = frame[:, :, ::-1]
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-    face_locations = face_recognition.face_locations(rgb_small_frame)
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+    # Display the results
     for (top, right, bottom, left) in face_locations:
-       cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 3
+        right *= 3
+        bottom *= 3
+        left *= 3
 
-       cv2.imshow('Video', frame)
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    # Display the resulting image
+    cv2.imshow('Video', frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):

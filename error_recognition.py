ad2ff60c0222624af3ac821b02288520085ebbff
shachardavid <shachadavid2013@gmail.com>
2022-12-23 04:07:50 +0200

    finished class and all axis check

--- error_recognition.py
+++ error_recognition.py
@@ -5,57 +5,94 @@ import cv2
 import numpy as np
 
 
-def get_smaller_frame(frame, ratio=0.33):
-    small_frame = cv2.resize(frame, None, fx=ratio, fy=ratio)
-    return small_frame[:, :, ::-1]
+class FaceMovementRecognition:
+    def __init__(self, calibration, xy_tolerance, z_tolerance=0.4) -> None:
+        self.ztol = z_tolerance
+        self.xytol = xy_tolerance
+        self.cal = calibration
+        self.video_capture = cv2.VideoCapture(0)
+
+    def __take_samples(self, amount):
+        frames = [self.video_capture.read()[1] for i in range(amount)]
+        self.video_capture.release()
+        return frames
+
+    def __get_faces_locations(self, frames):
+        return [face_recognition.face_locations(frame)[0] for frame in frames
+                if face_recognition.face_locations(frame)]
+
+    def __get_average_differance(self, locations, compute, get_diff):
+        locations = [compute(location) for location in locations]
+        d_cal = compute(self.cal)
+        return np.average([get_diff(location, d_cal) for location in locations])
+
+    def __get_average_distance(self, locations):
+        self.__get_average_differance(locations, FaceMovementRecognition.__get_face_center,
+                                      FaceMovementRecognition.get_face_distance)
+
+    def __get_average_ratio(self, locations):
+        self.__get_average_differance(locations, FaceMovementRecognition.__get_rect_size,
+                                      lambda a, b: a / b)
+
+    def check_z_movement(self, locations):
+        ratio = self.__get_average_ratio(locations)
+        if ratio < 1 - self.ztol:
+            return "Sit Closer"
+        if ratio > 1 + self.ztol:
+            return "Sit Further"
+        return "Damn You Good"
+
+    def check_xy_movement(self, locations):
+        x, y = self.__get_average_distance(locations)
+        if x > self.xytol:
+            x_msg = f"Right: {x}\n"
+        elif x < -self.xytol:
+            x_msg = f"Left: {x}\n"
+        if y > self.xytol:
+            y_msg = f"Up: {y}\n"
+        elif y < self.xytol:
+            y_msg = f"Down: {y}\n"
+        return (x_msg, y_msg) if x_msg and y_msg else ("Damn Bro", "Damn Bro")
+
+    def is_sitting_wrong(self):
+        frames = self.__take_samples(10)
+        locations = self.__get_faces_locations(frames)
+        return {'z': self.check_z_movement(locations),
+                'x': self.check_xy_movement(locations)[0],
+                'y': self.check_xy_movement(locations)[1]}
+
+    @staticmethod
+    def __get_face_center(location):
+        t, r, b, l = location
+        return np.array([(r - l) / 2, (t - b) / 2])
+
+    @staticmethod
+    def __get_rect_size(location):
+        t, r, b, l = location
+        return abs(r - l) * abs(t - b)
+
+    @staticmethod
+    def get_face_distance(face1, face2):
+        return [f1 - f2 for f1, f2 in zip(face1, face2)]
 
 
-def take_samples(amount):
-    frames = []
-    video_capture = cv2.VideoCapture(0)
-    frames = [video_capture.read()[1] for i in range(amount)]
-    # small_frame = get_smaller_frame(frame)
-    video_capture.release()
-    return frames
-
-
-def get_faces_locations(frames):
-    return [face_recognition.face_locations(frame)[0] for frame in frames
-            if face_recognition.face_locations(frame)]
-
-
-def get_face_center(location):
-    t, r, b, l = location
-    return np.array([(r - l) / 2, (t - b) / 2])
-
-
-def get_face_distance(face, cal):
-    return np.linalg.norm(face - cal)
-
-
-def get_average_distance(locations, cal):
-    locations = [get_face_center(location) for location in locations]
-    cal = get_face_center(cal)
-    return np.average([get_face_distance(location, cal) for location in locations])
-
-
-def is_sitting_wrong(tol, cal):
-    frames = take_samples(10)
-    locations = get_faces_locations(frames)
-    cal = read_json(cal)
-    dist = get_average_distance(locations, cal)
-    return dist > tol
-
-
-def read_json(path):
+def read_calibration_data(path):
     data = {}
     with open(path, 'r') as f:
         data = np.array(json.load(f)['position'])
     return data
 
 
+def get_smaller_frame(frame, ratio=0.33):
+    small_frame = cv2.resize(frame, None, fx=ratio, fy=ratio)
+    return small_frame[:, :, ::-1]
+
+
 def main():
+    cal = read_calibration_data('data.json')
+    rec = FaceMovementRecognition(cal, 20, 0.2)
     while True:
-        if is_sitting_wrong(4):
-            notification(title, message)
+        status = rec.is_sitting_wrong()
+
+        notification(title, message)
         sleep(300)
